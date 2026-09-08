def7f014e89bac1d807702b0cf5972099b41b36f
DESKTOP-EJUAGPG\MSI <tranquanghuy31072006@gmail.com>
2026-09-08 08:59:25 +0700

    Init MyWebsiteProject

---
+++ src/main/java/com/learning/mywebsiteproject/MyWebsiteProjectApplication.java
@@ -0,0 +1,13 @@
+package com.learning.mywebsiteproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyWebsiteProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyWebsiteProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/learning/mywebsiteproject/MyWebsiteProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.mywebsiteproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyWebsiteProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
